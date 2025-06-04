c4414182c750d99401e3286777b94f67bd5ba5ac
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2025-06-04 17:14:49 +0530

    Spring boot project demo

---
+++ src/main/java/com/priyanshu/SpringbootProjectDemo/HelloController.java
@@ -0,0 +1,14 @@
+package com.priyanshu.SpringbootProjectDemo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(path = "/hello")
+    public String helloWorld(){
+        return "Hello Priyanshu";
+    }
+}
